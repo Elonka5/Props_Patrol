56517cd7318b3057f9da03073c650570846c1d32
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2023-09-25 18:45:20 +0300

    Summary Layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout.jsx/Layout';
 
 export const App = () => {
   <Routes>
-    <Route path="/" element={<Header />} />
+    <Route path="/" element={<Layout />} />
   </Routes>;
 };
