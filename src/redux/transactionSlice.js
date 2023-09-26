effb98c52966614359768be5bc1338b144c54b48
gostraa <mcherkashina77@gmail.com>
2023-09-26 15:04:20 +0200

    slice and operations for transactions

---
+++ src/redux/transactionSlice.js
@@ -0,0 +1,87 @@
+import { createSlice } from '@reduxjs/toolkit';
+import {
+  addNewTransactionThunk,
+  deleteTransactionThunk,
+  fetchCategoriesThunk,
+  fetchCurrencyRatesThunk,
+  fetchTransactionsSummaryThunk,
+  getTransactionsThunk,
+  updateTransactionThunk,
+} from './transactionsOperations';
+
+const initialState = {
+  transactions: [],
+  transactionsCategories: null,
+  currencyRates: [],
+  transactionSummary: [],
+  isLoading: false,
+  isError: false,
+};
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleFulfilled = (state, { payload }) => {
+  state.isLoading = false;
+  state.transactions.push(payload);
+};
+
+const handleRejected = state => {
+  state.isLoading = false;
+  state.isError = true;
+};
+
+const handleFulfilledDelete = (state, { payload }) => {
+  state.isLoading = false;
+  state.transactions = state.transactions.filter(
+    transaction => transaction.id !== payload.id
+  );
+};
+const handleFulfilledCategories = (state, { payload }) => {
+  state.isLoading = false;
+  state.transactionsCategories = payload;
+};
+
+const handleFulfilledTransactionsSummary = (state, { payload }) => {
+  state.isLoading = false;
+  state.transactionSummary.push(payload);
+};
+
+const handleFulfilledCurrencyRates = (state, { payload }) => {
+  state.isLoading = false;
+  state.currencyRates.push(payload);
+};
+const transactionSlice = createSlice({
+  name: 'transactions',
+  initialState,
+  extraReducers: builder => {
+    builder
+      .addCase(addNewTransactionThunk.pending, handlePending)
+      .addCase(addNewTransactionThunk.fulfilled, handleFulfilled)
+      .addCase(addNewTransactionThunk.rejected, handleRejected)
+      .addCase(getTransactionsThunk.pending, handlePending)
+      .addCase(getTransactionsThunk.fulfilled, handleFulfilled)
+      .addCase(getTransactionsThunk.rejected, handleRejected)
+      .addCase(updateTransactionThunk.pending, handlePending)
+      .addCase(updateTransactionThunk.fulfilled, handleFulfilled)
+      .addCase(updateTransactionThunk.rejected, handleRejected)
+      .addCase(deleteTransactionThunk.pending, handlePending)
+      .addCase(deleteTransactionThunk.fulfilled, handleFulfilledDelete)
+      .addCase(deleteTransactionThunk.rejected, handleRejected)
+      .addCase(fetchCategoriesThunk.pending, handlePending)
+      .addCase(fetchCategoriesThunk.fulfilled, handleFulfilledCategories)
+      .addCase(fetchCategoriesThunk.rejected, handleRejected)
+      .addCase(fetchTransactionsSummaryThunk.pending, handlePending)
+      .addCase(
+        fetchTransactionsSummaryThunk.fulfilled,
+        handleFulfilledTransactionsSummary
+      )
+      .addCase(fetchTransactionsSummaryThunk.rejected, handleRejected)
+      .addCase(fetchCurrencyRatesThunk.pending, handlePending)
+      .addCase(fetchCurrencyRatesThunk.fulfilled, handleFulfilledCurrencyRates)
+      .addCase(fetchCurrencyRatesThunk.rejected, handleRejected);
+  },
+});
+
+export const transactionReducer = transactionSlice.reducer;
