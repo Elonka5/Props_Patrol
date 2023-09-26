285b8c7d098d9886916de58197100da1fd683e1b
gostraa <mcherkashina77@gmail.com>
2023-09-26 18:25:02 +0200

    home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import style from './HomePage.module.css';
+import TransactionList from 'components/TransactionList/TransactionList';
 
 const Home = () => {
   return (
-    <section className="home_section">
-      <div className="transaction_main_wrapper">
-        <div className="list_header">
+    <section className={style.home_section}>
+      <div className={style.transaction_main_wrapper}>
+        <div className={style.list_header}>
           <div>Date</div>
           <div>Type</div>
           <div>Category</div>
           <div>Comment</div>
           <div>Sum</div>
+        </div>
+        <div className={style.transaction_list_wrapper}>
+          <TransactionList />
         </div>
       </div>
     </section>
