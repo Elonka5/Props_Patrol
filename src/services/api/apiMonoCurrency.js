64b3d030c156851820a383229a355f70cc67dd42
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2023-09-26 21:11:35 +0300

    Update apiMonoCurrency.js

--- src/services/api/apiMonoCurrency.js
+++ src/services/api/apiMonoCurrency.js
@@ -2,6 +2,5 @@ import axios from 'axios';
 
 export async function fetchMonoCurrency() {
   const { data } = await axios.get('https://api.monobank.ua/bank/currency');
-  // console.log(data);
   return data;
 }
