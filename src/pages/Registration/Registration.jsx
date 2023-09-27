4712419a3098a71154d52f3d1dca4bc61d17d11f
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2023-09-27 19:44:35 +0300

    Update Registration.jsx

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -4,9 +4,7 @@ import { RegisterPageWrapper } from './Registration.styled';
 const Registration = () => {
   return (
     <RegisterPageWrapper>
-      {/* <div style={{ overflowY: 'auto', height: '100%' }}> */}
       <RegisterForm />
-      {/* </div> */}
     </RegisterPageWrapper>
   );
 };
