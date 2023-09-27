9cdfee96f05d62502caa4b5511e718ab80a6dcac
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2023-09-27 10:13:00 +0300

    change header

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,4 @@
 import Header from 'components/Header/Header';
-import Home from 'pages/Home/Home';
 import React, { Suspense } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 
@@ -11,7 +10,6 @@ const Dashboard = () => {
           <Header />
           <NavLink to="/home">Home</NavLink>
           <NavLink to="/statistics">Statistics</NavLink>
-          <Home />
           <Outlet />
         </Suspense>
       </main>
